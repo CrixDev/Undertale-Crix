306ad59ba5730952c8204268805de1d810d7c02f
CrixDev <devoracristian100@gmail.com>
2025-01-01 12:54:33 -0700

    Setup Page

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+function App() {
+
+  return (
+    <>
+<body>
+    <h1>Hola Soy Crix</h1>
+    esto esta bien verga jajaja FGRDSGS
+    <h2>fwasdwasdwasd</h2>
+    <h3>wasdwad</h3>
+    <h4>wasdwasd</h4>
+    <h5>wasdwasd</h5>
+    <h6>wasdwasd</h6>
+    <button class="btn-primary">
+        Hola
+    </button>
+<small>
+    que gracioso escribir como en undertale
+</small>
+    
+</body>
+
+    </>
+  )
+}
+
+export default App
