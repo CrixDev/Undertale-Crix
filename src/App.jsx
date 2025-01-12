11ecb3780d9796cc576c0e4a82d1535b6b39877b
CrixDev <devoracristian100@gmail.com>
2025-01-12 13:50:05 -0700

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,22 @@ import crix from "./CrixSans.png"
 function App() {
 
   return (
-    <div className="bg-black" >
+    <div className="bg-black px-20" >
 
       {/*HERO*/}    
     
       <div className="flex p-3 bg-black space-x-3 h-[500px]">
 
-        
-          <img className="w-1/2 h-full" src={panel} alt="" />
-          
+      <div className="flex justify-center bg-transparent w-1/2 h-full" >
+          <img className="w-full h-full" src={panel} alt="" />
+          </div>
           <div className="flex justify-center bg-transparent w-1/2 h-full" >
           <img className="w-96 h-full" src={crix} alt="" />
           
           </div>
-          
-          <img className="scale-x-[-1] w-1/2 h-full " src={panel} alt="" />
-          
+          <div className="flex justify-center bg-transparent w-1/2 h-full" >
+          <img className="scale-x-[-1] w-full h-full " src={panel} alt="" />
+          </div>
       </div>
 
       
@@ -28,7 +28,8 @@ function App() {
       <div className="bg-Black h-72 border-[14px] " >
       
       <h1 className="text-start text-4xl">Hola Soy Crix</h1>
-     
+      
+   
       </div>
 
       <div className="h-2 py-28">
@@ -37,6 +38,9 @@ function App() {
       <button class="btn-primary">
         Hola
     </button>
+    <button className="font-medium bg-black text-white border-[5px] hover:border-[#ffff00] hover:text-[#ffff00] ">
+        adios
+      </button>
   
     </div>
   )
